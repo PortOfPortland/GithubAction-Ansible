8f8ad58ac68d2e503f4f71ae1c2f95b0351700f1
Ray Terrill <rayterrill@users.noreply.github.com>
2021-06-10 21:22:55 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
+  
+#parse cmd arguments
+while getopts p:i:v:e: flag
+do
+    case "${flag}" in
+        p) playbook=${OPTARG};;
+        i) inventory=${OPTARG};;
+        v) vault_pass=${OPTARG};;
+        e) extra_vars=${OPTARG};;
+    esac
+done
 
-playbook=$1
-inventory=$2
-vault_pass=$3
-extra_vars=$4
+CMD="ansible-playbook"
 
-echo $vault_pass > .vaultpass
-
-if [ ! -z ${extra_vars} ]
+#check for playbook
+if [ ! -z $playbook ]
 then
-   ansible-playbook $playbook --inventory $inventory --vault-password-file .vaultpass --extra-vars $extra_vars 
-else
-   ansible-playbook $playbook --inventory $inventory --vault-password-file .vaultpass --extra-vars $extra_vars 
+   CMD="${CMD} ${playbook}"
 fi
+
+#check for inventory 
+if [ ! -z $inventory ]
+then
+   CMD="${CMD} --inventory ${inventory}"
+fi
+
+#check for vault_pass 
+if [ ! -z $vault_pass ]
+then
+   echo ${vault_pass} > .vaultpass
+   CMD="${CMD} --vault-password-file .vaultpass"
+fi
+
+#check for extra_vars 
+if [ ! -z $extra_vars ]
+then
+   CMD="${CMD} --extra-vars ${extra_vars}"
+fi
+
+#run the command
+${CMD}
