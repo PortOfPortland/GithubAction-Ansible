b8184def5028d23638175b467c096dac5bc42116
Ray Terrill <ray.terrill@portofportland.com>
2021-06-10 20:10:31 -0700

    Added ability to pass extra vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,13 @@
 playbook=$1
 inventory=$2
 vault_pass=$3
+extra_vars=$4
 
 echo $vault_pass > .vaultpass
 
-ansible-playbook deploy.yml --vault-password-file .vaultpass 
+if [ ! -z ${extra_vars} ]
+then
+   ansible-playbook $playbook --inventory $inventory --vault-password-file .vaultpass --extra-vars $extra_vars 
+else
+   ansible-playbook $playbook --inventory $inventory --vault-password-file .vaultpass --extra-vars $extra_vars 
+fi
